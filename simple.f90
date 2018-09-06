bfe1ffaf7acb8e43f469826e66ed16bf315b974b
Beatrice Baldelli <beatriba@fys.uio.no>
2018-09-06 12:13:44 +0200

    Finished algorithm. It works.

--- simple.f90
+++ simple.f90
@@ -12,12 +12,39 @@ implicit none
       end function
    end interface
 
-  integer :: expn, i
+   integer :: expn, i, n
+   real(8) :: h, h_sq        
+   real(8), dimension(:), allocatable :: d, solution, b, analytic
 
    print *, "Exponent:"
    read(*,*) expn
 
-   print *, f(expn*1d0), exact(expn*1d0)  
+   n = 10d0 ** expn
+   h = 1d0 / n
+   h_sq = h * h
+ 
+   allocate(d(1:n-1), solution(0:n), b(1:n-1), analytic(0:n))
+
+   solution(0) = 0; solution(n) = 0
+   do i = 1, n-1
+      d(i) = (1d0 + i) / i
+      b(i) = h_sq * f(i*h)
+   end do
+   
+   do i = 2, n-1
+      b(i) = b(i) + b(i-1) / d(i-1)
+   end do
+      solution(n-1) = b(n-1) / d(n-1)
+   do i = n-1, 2, -1
+      solution(i-1) = (solution(i) + b(i-1)) / d(i-1)
+   end do
+   do i = 0, n
+      analytic(i) = exact(i*h)
+   end do
+
+do i = 10, 20
+   print *, solution(i), analytic(i)
+end do
 end program
 
 pure function f(x)
